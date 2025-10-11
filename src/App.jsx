9aa9deaa2b7f06acb3c45a635bd03f53f4bd3862
ink <92540444+GreatLoveV@users.noreply.github.com>
2025-10-11 19:55:21 +0100

    do give feedback exercice and complete all required components

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,76 @@
-import  {useState} from 'react'
+import {useState}  from "react";
 
-const Display = ({counter}) => <div>{counter}</div>
-
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const StatisticLine = ({text, value}) =>{
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
 }
+const Statistics = (props)=>{
+  if (props.totalClicks === 0)
+    { 
+      return (
+        <div>
+          <p>No feedback given</p>
+        </div>
+      )
+    } 
 
-const Button = ({onClick, text})=> <button onClick = {onClick}>{text}</button>
-
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = ()=> {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = ()=> {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1);
-  }
-
-  const setToZero = ()=> {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0);
-  }
-  
   return (
     <div>
-      <Display counter = {counter}/>
-      <Button onClick = {increaseByOne} text = "plus"/>
-      <Button onClick = {decreaseByOne} text = "minus"/>
-      <Button onClick = {setToZero} text = "zero"/>
+      <StatisticLine text = {'good'} value={props.good}/>
+      <StatisticLine text = {'neutral'} value={props.neutral}/>
+      <StatisticLine text = {'bad'} value={props.bad}/>
+      <StatisticLine text = {'all'} value={props.totalClicks}/>
+      <StatisticLine text = {'average'} value={props.average}/>
+      <StatisticLine text = {'positive'} value={props.positiveFeedback + '%'}/>
+
     </div>
-
   )
-  };
+}
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [totalClicks, setTotalClicks] = useState(0)
 
-  export default App;
+  const handleGoodCLick = () =>{
+    console.log("Good was clicked!")
+    setGood(good + 1)
+    setTotalClicks(totalClicks +1)
+  }
+  const handleNeutralCLick = () =>{
+    console.log("Neutral was clicked!")
+    setNeutral(neutral + 1)
+    setTotalClicks(totalClicks +1)
+  }
+  const handleBadCLick = () =>{
+    console.log("Bad was clicked!")
+    setBad(bad + 1)
+    setTotalClicks(totalClicks +1)
+    
+  }
+  const average = totalClicks === 0
+    ? 0 
+    : (good * 1 + neutral *0 + bad * -1)/ totalClicks;
+  const positiveFeedback = totalClicks === 0
+    ? "0%"
+    : (good /totalClicks)*100
+  return(
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodCLick} text={"good"}/>
+      <Button onClick={handleNeutralCLick} text={"neutral"}/>
+      <Button onClick={handleBadCLick} text={"bad"}/>
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} totalClicks={totalClicks} average={average} positiveFeedback = {positiveFeedback} /> 
+
+      
+    </div>
+  )
+}
+
+export default App
