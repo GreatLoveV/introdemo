abc4ef8c04237b1e29bc061bdf9eeb76ed3b2836
ink <92540444+GreatLoveV@users.noreply.github.com>
2025-10-17 18:05:44 +0100

    create components directory and import Note.jsx which uses maps keys and arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
-import {useState} from 'react';
+import Note from './components/Note'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
-  const handleRandomSelect = () =>{
-    const randomIndex = Math.floor(Math.random()*anecdotes.length)
-    console.log(randomIndex)
-    return setSelected(randomIndex) 
-  }
-  const handleAnecdoteVote = () => {
-    const copy = [...votes]
-    copy[selected] =  copy[selected] + 1
-    console.log(selected)
-    return setVotes(copy)
-  }
 
-  const mostVoted = () =>{
-    const highestNumber = Math.max(...votes);
-    const highestNumIndex = votes.indexOf(highestNumber)
-    return anecdotes[highestNumIndex]
-  }
-
-  return (
+const App = ({notes}) => {
+  return(
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>{votes[selected]}</p>
-      <button onClick={handleRandomSelect}>next anecdote</button>
-      <button onClick={handleAnecdoteVote}>vote</button>
-
-      <h1>Anecdote with most votes</h1>
-      <p>{mostVoted()}</p>
+      <h1>Notes</h1>
+      <ul>
+          {notes.map(note => <Note key={note.id } note = {note} />)}  
+      </ul>
     </div>
   )
 }
