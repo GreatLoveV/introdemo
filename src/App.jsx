acbf3bd9b3b949eb1bdd6507009af0ed1aaf732f
ink <bjaghet2014@gmail.com>
2025-11-08 12:13:34 +0100

    add both styles using both index.css and inline styles add Notification and footer Add styled Notification error state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {useState, useEffect} from 'react'
 import Note from './components/Note'
+import Footer from './components/Footer'
+import Notification from './components/Notification'
 import noteService from './services/notes'
+
 
 
 
@@ -8,6 +11,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(()=>{
     noteService
@@ -27,9 +31,13 @@ const App = () => {
         setNotes(notes.map(note => note.id === id ? returnedNote : note))
       })
       .catch(error =>{
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        console.error('Error updating note:', error)
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n=> n.id !== id))
       })
   }
@@ -60,6 +68,7 @@ const App = () => {
   return(
     <div>
       <h1>Notes</h1>
+      <Notification message = {errorMessage}/>
       <div>
         <button onClick={()=>setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -76,6 +85,8 @@ const App = () => {
         onChange={handleNoteChange}/>
         <button type='submit'>save</button>
       </form>
+
+      <Footer />
     </div>
   )
 }
